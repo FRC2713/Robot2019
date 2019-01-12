2c7443e15d5b62892c48e2802172715f71ca15f1
Dara Casey <dara.casey14@gmail.com>
2019-01-11 20:17:03 -0500

    Added OI class with xbox and arcade controllers
    Co-authored-by: Conlan Cesar <conlanc@gmail.com>

--- robot/src/main/java/org/iraiders/robot2019/robot/OI.java
+++ robot/src/main/java/org/iraiders/robot2019/robot/OI.java
@@ -1,35 +1,80 @@
 package org.iraiders.robot2019.robot;
 
-/**
- * This class is the glue that binds the controls on the physical operator
- * interface to the commands and command groups that allow control of the robot.
- */
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+
 public class OI {
-  //// CREATING BUTTONS
-  // One type of button is a joystick button which is any button on a
-  //// joystick.
-  // You create one by telling it which joystick it's on and which button
-  // number it is.
-  // Joystick stick = new Joystick(port);
-  // Button button = new JoystickButton(stick, buttonNumber);
+  public static XboxController xBoxController;
+  public static Joystick arcadeController;
+  private static Joystick leftAttack;
+  private static Joystick rightAttack;
 
-  // There are a few additional built in buttons you can use. Additionally,
-  // by subclassing Button you can create custom triggers and bind those to
-  // commands the same as any other Button.
+  // Controllers
+  public static final int BACKUP_XBOX_PORT = 0;
+  public static final int BACKUP_ARCADE_PORT = 1;
+  public static final int ATTACK_LEFT_PORT = 2;
+  public static final int ATTACK_RIGHT_PORT = 3;
+  public static final String XBOX_NAME = "Controller (XBOX 360 For Windows)";
+  public static final String ARCADE_NAME = "Mayflash Arcade Stick";
+  public static final String ATTACK_NAME = "Logitech Attack 3";
 
-  //// TRIGGERING COMMANDS WITH BUTTONS
-  // Once you have a button, it's trivial to bind it to a button in one of
-  // three ways:
+  public OI() {
+    initControllers();
+  }
 
-  // Start the command when the button is pressed and let it run the command
-  // until it is finished as determined by it's isFinished method.
-  // button.whenPressed(new ExampleCommand());
+  /**
+   * Scans all (7) controller ports and assigns them via known names
+   */
+  private void initControllers() {
+    // TODO Use generics
+    for (int i = 0; i < 6; i++) {
+      Joystick test = new Joystick(i);
+      if (test.getName().equals(XBOX_NAME)) {
+        xBoxController = new XboxController(i);
+      } else if (test.getName().equals(ARCADE_NAME)) {
+        arcadeController = new Joystick(i);
+      }
+    }
+    if (xBoxController == null) {
+      xBoxController = new XboxController(BACKUP_XBOX_PORT);
+    }
+    if (arcadeController == null) {
+      arcadeController = new Joystick(BACKUP_ARCADE_PORT);
+    }
+    leftAttack = new Joystick(ATTACK_LEFT_PORT);
+    rightAttack = new Joystick(ATTACK_RIGHT_PORT);
+  }
 
-  // Run the command while the button is being held down and interrupt it once
-  // the button is released.
-  // button.whileHeld(new ExampleCommand());
+  /**
+   * Rumbles a given controller for a specified time
+   * @param stick Controller to rumble
+   * @param ms Time in Milliseconds
+   */
+  public static void rumbleController(GenericHID stick, double intensity, int ms) {
+    rumbleController(stick, intensity, ms, GenericHID.RumbleType.kLeftRumble);
+  }
 
-  // Start the command when the button is released and let it run the command
-  // until it is finished as determined by it's isFinished method.
-  // button.whenReleased(new ExampleCommand());
+  /**
+   * Rumbles a given controller for a specified time
+   * Left rumble is like an earthquake, right rumble is like a vibrating toothbrush
+   * @param stick Controller to rumble
+   * @param ms Time in Milliseconds
+   * @param rumbleType Type of rumble to use
+   */
+  public static void rumbleController(GenericHID stick, double intensity, int ms, GenericHID.RumbleType rumbleType) {
+    if (ms > 0) {
+      new Thread(() -> {
+        _setRumble(stick, intensity, rumbleType);
+        try { Thread.sleep(ms); } catch (InterruptedException ignored) {}
+        _setRumble(stick, 0, rumbleType);
+      }).start();
+    } else {
+      _setRumble(stick, intensity, rumbleType);
+    }
+  }
+
+  private static void _setRumble(GenericHID stick, double intensity, GenericHID.RumbleType rumbleType) {
+    stick.setRumble(rumbleType, intensity);
+  }
 }
