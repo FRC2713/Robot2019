623feb1a02a07db0e29d52ebdc288a369ef217e1
Marissa Zelten <mmzelten@gmail.com>
2019-02-06 20:27:38 -0500

    Redid BallIntakeSubsystem, prepped for hatch intake

--- robot/src/main/java/org/iraiders/robot2019/robot/subsystems/IntakeSubsystem.java
+++ robot/src/main/java/org/iraiders/robot2019/robot/subsystems/IntakeSubsystem.java
@@ -1,35 +1,28 @@
 package org.iraiders.robot2019.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import org.iraiders.robot2019.robot.OI;
 import org.iraiders.robot2019.robot.RobotMap;
-import org.iraiders.robot2019.robot.commands.ResponsiveMotorCommand;
-import org.iraiders.robot2019.robot.commands.SimpleMotorCommand;
-import org.iraiders.robot2019.robot.commands.intake.GrabberControlCommand;
+import org.iraiders.robot2019.robot.commands.intake.BallIntakeControlCommand;
 
 public class IntakeSubsystem extends Subsystem {
-  private final WPI_TalonSRX grabberIntakeRight = new WPI_TalonSRX(RobotMap.grabberIntakeRightTalonPort);
-  private final WPI_TalonSRX grabberIntakeLeft = new WPI_TalonSRX(RobotMap.grabberIntakeLeftTalonPort);
-  public final WPI_TalonSRX grabberIntakeJoint = new WPI_TalonSRX(RobotMap.grabberIntakeJointTalonPort);
+  public final BallIntakeControlCommand ballIntakeControlCommand = new BallIntakeControlCommand();
 
-  public final DoubleSolenoid grabberSolenoid = new DoubleSolenoid(RobotMap.grabberOpenNodeId, RobotMap.grabberCloseNodeId);
-
+  public final DoubleSolenoid ballIntakeSolenoid = new DoubleSolenoid(RobotMap.ballIntakeUpNodeId, RobotMap.ballIntakeDownNodeId);
   private final DigitalInput ballIntakeLimitSwitch = new DigitalInput(RobotMap.ballIntakeLimitSwitchPort);
+  // TODO stop intake roller and bring up when ^^^^ is triggered
 
   public IntakeSubsystem() {
-    grabberIntakeLeft.set(ControlMode.Follower, RobotMap.grabberIntakeRightTalonPort);
     initControls();
   }
 
   private void initControls() {
     GenericHID buttonBox = OI.arcadeController;
 
+    /*
     JoystickButton btn3 = new JoystickButton(buttonBox, 3);
     JoystickButton btn4 = new JoystickButton(buttonBox, 4);
     JoystickButton btn5 = new JoystickButton(buttonBox, 5);
@@ -37,23 +30,20 @@ public class IntakeSubsystem extends Subsystem {
     JoystickButton btn7 = new JoystickButton(buttonBox, 7);
 
 
-    btn3.whileHeld(new ResponsiveMotorCommand(grabberIntakeRight,.75, ballIntakeLimitSwitch));
-    btn4.whileHeld(new SimpleMotorCommand(grabberIntakeRight,-.75));
+    btn3.whileHeld(new ResponsiveMotorCommand(ballIntakeMotor,.75, ballIntakeLimitSwitch));
+    btn4.whileHeld(new SimpleMotorCommand(ballIntakeMotor,-.75));
     btn5.whileHeld(new SimpleMotorCommand(grabberIntakeJoint,.5));
-    btn6.whileHeld(new GrabberControlCommand(this, GrabberPosition.OPEN));
-    btn7.whileHeld(new GrabberControlCommand(this, GrabberPosition.CLOSE));
+    btn6.whileHeld(new BallIntakeJointCommand(this, GrabberPosition.OPEN));
+    btn7.whileHeld(new BallIntakeJointCommand(this, GrabberPosition.CLOSE));
+    */
   }
 
   @Override
   protected void initDefaultCommand() {
-
+    ballIntakeControlCommand.start();
   }
 
-  public enum IntakePosition {
-    STARTING
-  }
-
-  public enum GrabberPosition {
-    OPEN, CLOSE
+  public enum IntakeJointPosition {
+    UP, DOWN
   }
 }
