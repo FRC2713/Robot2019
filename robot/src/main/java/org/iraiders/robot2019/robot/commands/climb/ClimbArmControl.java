70237d8da84aa5cb92e0b78ebf36dea4b854965f
JHG777000 <jhg777000@gmail.com>
2019-03-30 09:40:40 -0400

    set setInputRange to 0 to 1

--- robot/src/main/java/org/iraiders/robot2019/robot/commands/climb/ClimbArmControl.java
+++ robot/src/main/java/org/iraiders/robot2019/robot/commands/climb/ClimbArmControl.java
@@ -12,7 +12,7 @@ public class ClimbArmControl extends PIDCommand {
   public ClimbArmControl(ClimbSubsystem climbSubsystem) {
     super(.05, 0, 0); // TODO Tune this and max & min range
     this.cs = climbSubsystem;
-    this.setInputRange(-1, 1);
+    this.setInputRange(0, 1);
     this.getPIDController().setPercentTolerance(5);
   }
 
