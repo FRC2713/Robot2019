2e761cf4a94b899dcb26cd7492e2056042e4f485
Dara Casey <dara.casey14@gmail.com>
2019-06-08 15:57:18 -0400

    Created code for checking if ball was in before stopping motors

--- robot/src/main/java/org/iraiders/robot2019/robot/commands/intake/BallIntakeMonitor.java
+++ robot/src/main/java/org/iraiders/robot2019/robot/commands/intake/BallIntakeMonitor.java
@@ -7,6 +7,7 @@ import org.iraiders.robot2019.robot.subsystems.IntakeSubsystem;
 public class BallIntakeMonitor extends Command {
   private IntakeSubsystem intakeSubsystem;
   private double firstDetectedTime = 0;
+  private boolean isIn = false;
 
   public BallIntakeMonitor(IntakeSubsystem intakeSubsystem){
     this.intakeSubsystem = intakeSubsystem;
@@ -14,14 +15,16 @@ public class BallIntakeMonitor extends Command {
 
   @Override
   protected void execute() {
-    if (!intakeSubsystem.ballIntakeLightSensor.get() || intakeSubsystem.intakeTalon.getOutputCurrent() >= 100) {
+    if ((!intakeSubsystem.ballIntakeLightSensor.get() && !isIn) || (intakeSubsystem.intakeTalon.getOutputCurrent() >= 100)) {
       if (firstDetectedTime == 0) firstDetectedTime = Timer.getFPGATimestamp();
 
-      if (Timer.getFPGATimestamp() - firstDetectedTime >= .5) {
+      if (Timer.getFPGATimestamp() - firstDetectedTime >= .5 && (!isIn || intakeSubsystem.intakeTalon.getOutputCurrent() >= 100)) {
         intakeSubsystem.ballIntakeJointCommand.setIntakeJointPosition(IntakeSubsystem.IntakeJointPosition.UP);
         intakeSubsystem.ballIntakeControlCommand.setBallIntakeState(BallIntakeControlCommand.BallIntakeState.STOPPED);
+        isIn = true;
       }
     } else {      firstDetectedTime = 0;
+                  isIn = false;
     }
   }
 
